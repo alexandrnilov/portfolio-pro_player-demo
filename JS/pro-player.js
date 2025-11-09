a071dc64957f3f9ec2f202f10b65aa23a99466f6
ppp <ppp@MacBook-Pro-ppp.local>
2025-11-10 02:14:47 +0300

    test 4

--- JS/pro-player.js
+++ JS/pro-player.js
@@ -1,7 +1,7 @@
-import WaveSurfer from '/JS/WAVE/wavesurfer.esm.js';
-import RegionsPlugin from '/JS/WAVE/wavesurfer.regions.esm.js';
-import Soundfont from '/JS/WAVE/soundfont-player.js';
-console.log(Soundfont);
+import WaveSurfer from './JS/WAVE/wavesurfer.esm.js';
+import RegionsPlugin from './JS/WAVE/wavesurfer.regions.esm.js';
+import Soundfont from './JS/WAVE/soundfont-player.js';
+//console.log(Soundfont);
 
 
 
